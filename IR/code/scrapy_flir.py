60cf4463a1b95bd816a710cff92ed5a7aeb8e0ec
toroumm <toroumm@gmail.com>
2017-05-25 17:30:27 -0300

    inclusao legenda e exobicao das imagens com matplot lib

--- IR/code/scrapy_flir.py
+++ IR/code/scrapy_flir.py
@@ -4,16 +4,24 @@ import cv2, io
 import PIL.Image as pil
 import random
 
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+
 '''
 Formula temperatura
 
 T = B / np.log(R1/(R2*(img+O))+F)
 
 '''
-#
+
+def mouse_event(event):
+
+	print  event.pickx, event.picky
+#*************************************************************************************************
 
 COLORS = [ (202, 201, 197),  # Light gray
-			(171, 9, 0),      # Rede
+			(171, 9, 0),      # Red
     			(166, 78, 46),    # Light orange
     			(255, 190, 67),   # Yellow
     			(163, 191, 63),   # Light green
@@ -24,10 +32,23 @@ COLORS = [ (202, 201, 197),  # Light gray
     			(237, 124, 60),    # orange
 		]
 
+#*************************************************************************************************
+
+def open_window(name,img):
+	try:
+		cv2.namedWindow(name)
+		cv2.resizeWindow(name, 300,300)
+		cv2.moveWindow(name, 0,0)
+		cv2.imshow(name, img)
+	except:
+		pass
+
+#*************************************************************************************************
+
 def next_color(color_list):
 		#Create a different color from a base color list.
 		
-		step = 1
+		step = 20
 
 		color = color_list[random.randint(0, len(color_list) - 1)]
 		while True:
@@ -35,8 +56,7 @@ def next_color(color_list):
 
 			yield map(lambda base: (base + step) % 256, color)
 
-			step += 197
-
+			step += 50
 
 #*************************************************************************************************
 
@@ -72,13 +92,30 @@ def get_temperature(param, data):
 
 path = '../images/'#'/media/sf_ownCloud/visao_computacional/FLIR/imagens/'
 
+scope = range(0,100,30)
 
-for i in range(10):
+color = []
 
-	print tuple(next(next_color(COLORS)))
+for i in range(len(scope)-1):
 
+	color.append(tuple(next(next_color(COLORS))))
 
-sys.exit()
+print color 
+
+legenda = np.zeros(((len(scope)-1)*30,75,3))
+
+k =0
+
+font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+for i in range(0,legenda.shape[0],30):
+
+	if k <= len(scope)-2:
+	
+		legenda[i:i+30,:,:] = color[k]
+
+		cv2.putText(legenda,str(scope[k])+'-'+str(scope[k+1]),(2,i+20), font, 1, (0,0,0), 1, cv2.LINE_AA)
+
+		k+=1
 
 for arquivo in os.listdir(path):
 	
@@ -90,10 +127,64 @@ for arquivo in os.listdir(path):
 
 		imgThermal =  get_temperature(param, raw)
 
-		print np.max(imgThermal), np.min(imgThermal), np.mean(imgThermal)
+		asd = np.zeros((imgThermal.shape[0], imgThermal.shape[1],3))
+
+		for i in xrange(0, imgThermal.shape[0]):
+			for j in xrange(0, imgThermal.shape[1]):
+				for k in xrange(1, len(scope)-1):
+					
+					if(imgThermal[i,j]>= scope[k-1] and imgThermal[i,j] < scope[k]):
+						asd[i,j,0] = color[k][0]
+
+						asd[i,j,1] = color[k][1]
+
+						asd[i,j,2] = color[k][2]
+						
+						break
+						
+		figure  = plt.figure()
 
 		
+
+		one = figure.add_subplot(1,3,1)
+
+		one.set_title('Original')
+
+		one.axis('off')
+
+		one.canvas.mpl_connect('pick_event', mouse_event)	
+
+		plt.imshow(cv2.imread(path+arquivo))
+
+		two = figure.add_subplot(1,3,2)
+
+		two.set_title('Segmentado')
+
+		two.axis('off')
 		
+		plt.imshow(asd)
+
+		tree = figure.add_subplot(1,3,3)
+
+		tree.set_title('Legenda')
+
+		tree.axis('off')
+
+		plt.imshow(legenda)
+
+		
+			
+		plt.show()
+
+		sys.exit()
+		#print np.max(imgThermal), np.min(imgThermal), np.mean(imgThermal)
+		#open_window('teste',asd)
+		#open_window('original',cv2.imread(path+arquivo))
+		#cv2.imwrite(path+'testes/'+arquivo[:-3]+'_teste.jpg',asd)
+
+		#cv2.waitKey(0)
+#*************************************************************************************************
+
 
 
 
